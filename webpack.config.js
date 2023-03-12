1173843e3815417b397bccc7caac6e8f8dff5828
amandaguan-ag <amandaguan1314@gmail.com>
2023-03-12 09:55:46 -0700

    fix webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,29 +1,29 @@
-const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const { CleanWebpackPlugin } = require('clean-webpack-plugin'); 
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 
-const ESLintPlugin = require('eslint-webpack-plugin');   !
+const ESLintPlugin = require("eslint-webpack-plugin");
 module.exports = {
-  entry: './src/index.js',
+  entry: "./src/index.js",
   output: {
-    filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist')
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "dist"),
   },
-  devServer: {                 
-    // contentBase: './dist'      
-    static: './'
-  },                           
-  devtool: 'eval-source-map',  
+  devServer: {
+    // contentBase: './dist'
+    static: "./",
+  },
+  devtool: "eval-source-map",
   plugins: [
-    new ESLintPlugin(), !
+    new ESLintPlugin(),
     new CleanWebpackPlugin({
-      verbose: true
+      verbose: true,
     }),
     new HtmlWebpackPlugin({
-      title: 'Shape Tracker',
-      template: './src/index.html',
-      inject: 'body'
-    })
+      title: "movie ticket",
+      template: "./src/index.html",
+      inject: "body",
+    }),
   ],
   module: {
     rules: [
@@ -31,27 +31,22 @@ module.exports = {
         test: /\.(gif|png|avif|jpe?g)$/,
         use: [
           {
-            loader: 'file-loader',
+            loader: "file-loader",
             options: {
-              name: '[name].[ext]',
-              outputPath: 'assets/images/'
-            }
-          }
-        ]
+              name: "[name].[ext]",
+              outputPath: "assets/images/",
+            },
+          },
+        ],
       },
       {
-        test:/\.html$/,
-        use: [
-          'html-loader'
-        ]
+        test: /\.html$/,
+        use: ["html-loader"],
       },
       {
         test: /\.css$/,
-        use: [
-          'style-loader',
-          'css-loader'
-        ]
-      }
-    ]
-  }
+        use: ["style-loader", "css-loader"],
+      },
+    ],
+  },
 };
